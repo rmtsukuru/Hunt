84c549f7ab1bad056b02755272b5786995383604
rmtsukuru <samcollegeemail@gmail.com>
2017-02-08 22:53:08 -0500

    Player's facing direction is now taken into account with position of sword attack.

--- src/player.js
+++ src/player.js
@@ -2,12 +2,20 @@ const PLAYER_SPEED = 5;
 const PLAYER_SIZE = 32;
 const PLAYER_SWORD_FRAMES = 30;
 
+const directions = {
+    left: 0,
+    right: 1,
+    up: 2,
+    down: 3
+};
+
 function Player(x, y) {
     Entity.call(this, x, y);
     this.width = this.height = PLAYER_SIZE;
     this.color = '#a22';
     this.swordDrawn = false;
     this.swordTimer = 0;
+    this.facing = directions.down;
 }
 
 Player.prototype = Object.create(Entity.prototype);
@@ -25,7 +33,19 @@ Player.prototype.update = function() {
     }
     else {
         if (keyState.z) {
-            var sword = new Sword(this.x + 20, this.y + 32);
+            var sword;
+            if (this.facing == directions.left) {
+                sword = new Sword(this.x - 32, this.y + 12, true);
+            }
+            else if (this.facing == directions.right) {
+                sword = new Sword(this.x + 32, this.y + 12, true);
+            }
+            else if (this.facing == directions.up) {
+                sword = new Sword(this.x + 12, this.y - 32);
+            }
+            else {
+                sword = new Sword(this.x + 20, this.y + 32);
+            }
             entities.unshift(sword);
             this.swordDrawn = true;
             this.swordTimer = PLAYER_SWORD_FRAMES;
@@ -42,6 +62,23 @@ Player.prototype.update = function() {
             }
             if (keyState.down || keyState.s) {
                 this.yVelocity += PLAYER_SPEED;
+            }
+
+            if (this.yVelocity == 0) {
+                if (this.xVelocity < 0) {
+                    this.facing = directions.left;
+                }
+                else if (this.xVelocity > 0) {
+                    this.facing = directions.right;
+                }
+            }
+            else if (this.xVelocity == 0) {
+                if (this.yVelocity < 0) {
+                    this.facing = directions.up;
+                }
+                else if (this.yVelocity > 0) {
+                    this.facing = directions.down;
+                }
             }
         }
     }
