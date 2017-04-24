43b88130771487e872aa26eefd599b644207e5a6
rmtsukuru <samcollegeemail@gmail.com>
2017-04-24 02:00:23 -0400

    Added extra frames for player's down-facing walk animation.

--- src/player.js
+++ src/player.js
@@ -1,6 +1,8 @@
 const PLAYER_SPEED = 5;
 const PLAYER_SIZE = 32;
 const PLAYER_SPRITE_MULTIPLIER = 0.5;
+const PLAYER_ANIMATION_FRAMES = 10;
+const PLAYER_FRAME_COUNT = 3;
 
 const directions = {
     left: 0,
@@ -15,9 +17,21 @@ function Player(x, y) {
     this.color = '#a22';
     this.swordDrawn = false;
     this.facing = directions.down;
+    this.frameTimer = PLAYER_ANIMATION_FRAMES;
+    this.animationFrame = 0;
+    this.preloadImages();
 }
 
 Player.prototype = Object.create(Entity.prototype);
+
+Player.prototype.preloadImages = function() {
+    for (var i = 0; i < PLAYER_FRAME_COUNT; i++) {
+        loadImage('player0' + i + '.png');
+    }
+    loadImage('player1.png');
+    loadImage('player2.png');
+    loadImage('player3.png');
+}
 
 Player.prototype.update = function() {
     this.xVelocity = 0;
@@ -74,11 +88,25 @@ Player.prototype.update = function() {
         }
     }
     handleTileCollision(this);
+    if (Math.abs(this.xVelocity) > 0 || Math.abs(this.yVelocity) > 0) {
+        if (this.frameTimer > 0 ) {
+            this.frameTimer--;
+        }
+        else {
+            this.frameTimer = PLAYER_ANIMATION_FRAMES;
+            this.animationFrame++;
+            this.animationFrame %= PLAYER_FRAME_COUNT;
+        }
+    }
+    else {
+        this.frameTimer = PLAYER_ANIMATION_FRAMES;
+        this.animationFrame = 0;
+    }
     Entity.prototype.update.call(this);
 };
 
 Player.prototype.draw = function() {
-    var image = 'player0.png';
+    var image;
     if (this.facing == directions.left) {
         image = 'player1.png';
     }
@@ -87,6 +115,9 @@ Player.prototype.draw = function() {
     }
     else if (this.facing == directions.right) {
         image = 'player3.png';
+    }
+    else {
+        image = 'player0' + this.animationFrame + '.png';
     }
     drawImage(image, this.x, this.y - (PLAYER_SIZE * PLAYER_SPRITE_MULTIPLIER));
 };
