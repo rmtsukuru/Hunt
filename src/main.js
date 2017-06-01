9b7351dc43fc08bf4e1db7e980f0a17b2d81ce36
rmtsukuru <samcollegeemail@gmail.com>
2017-06-01 00:34:44 -0400

    Added tile coordinate display to HUD.

--- src/main.js
+++ src/main.js
@@ -21,8 +21,9 @@ window.onload = function() {
 
     function drawHud() {
         drawText('X: ' + player.x + ' Y: ' + player.y, 2, 20, 'Cambria', '24px', '#2f2', true);
-        drawText('Beast HP: ' + beast.health, 2, 48, 'Cambria', '24px', '#2ff', true);
-        drawText('Beast Distance: ' + beast.distanceFromPlayer(), 2, 76, 'Cambria', '24px', '#e88', true);
+        drawText('Tile X: ' + tileIndex(player.x) + ' Tile Y: ' + tileIndex(player.y), 2, 48, 'Cambria', '24px', '#f2f', true);
+        drawText('Beast HP: ' + beast.health, 2, 76, 'Cambria', '24px', '#2ff', true);
+        drawText('Beast Distance: ' + beast.distanceFromPlayer(), 2, 104, 'Cambria', '24px', '#e88', true);
     }
 
     draw = function() {
