bea2a0816394d8d34da8f84953e00d049aa4b2cc
rmtsukuru <samcollegeemail@gmail.com>
2017-07-09 22:48:09 -0400

    Reduced BGM volume.

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,5 +1,6 @@
 const FPS = 60;
+const SPOTLIGHT_OFFSET = 1000;
+const BGM_VOLUME = 0.5;
 const MUTE_BGM = false;
 const MUTE_ECHOLOCATION = false;
-const SPOTLIGHT_OFFSET = 1000;
 const DEBUG_DAMAGE = true;
